103307d4f049db2016b25107d7c9a961d8c41030
bbosonbboson <jin.k.lee4@gmail.com>
2022-02-05 11:04:09 +0900

    라우터 설정

---
+++ src/pages/Korea/Korea.tsx
@@ -0,0 +1,14 @@
+import Summary from "../../Components/Summary"
+import TodaySummary from "../../Components/TodaySummary"
+
+
+const Korea = () => {
+    return (
+        <>
+            <Summary/>
+            <TodaySummary/>
+        </>
+    )
+}
+
+export default Korea
