798ebdf67169c6eb2313705f2ca00b610c51515d
bbosonbboson <jin.k.lee4@gmail.com>
2022-02-19 11:04:13 +0900

    백신 접종 현황 추가

--- src/httpAPI.ts
+++ src/httpAPI.ts
@@ -21,3 +21,9 @@ export const httpGetSummary = async (): Promise<SummaryType> => {
     cities: Object.values(cities)
   };
 };
+
+export const httpGetVaccineSummary = async () : Promise<VaccineSummaryType> => {
+  const { data } = await httpClient.get('/korea/vaccine/');
+
+  return data.korea
+}
